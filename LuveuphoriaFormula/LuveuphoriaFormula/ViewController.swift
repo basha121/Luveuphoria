35e5ef7d5b6120059f7b2fa766ec2766e7fcc64a
basha121 <basha.codegeeks@gmail.com>
2016-06-15 08:49:22 +0530

    Initial commit

---
+++ LuveuphoriaFormula/LuveuphoriaFormula/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LuveuphoriaFormula
+//
+//  Created by BhavikShah on 15/06/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
